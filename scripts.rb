9d73f7f40a7c7bf65ea9c3bba0ac92f68e6ca826
FrancoRosa <km115.franco@gmail.com>
2020-03-30 17:09:22 -0500

    Add block_given evaluation

--- scripts.rb
+++ scripts.rb
@@ -1,5 +1,43 @@
+# Adidional methods for Enumerables
 module Enumerable
-    def my_each
-      # your code here
+
+  def my_each
+    if block_given?
+      i = 0
+      while i < length
+        yield self[i]
+        i = i.next
+      end
     end
+    to_enum
   end
+
+  def my_each_with_index
+    if block_given?
+      i = 0
+      while i < length
+        yield self[i], i
+        i = i.next
+      end
+    end
+    to_enum
+  end
+
+  # def my_select &block 
+  #   result = []
+  #   my_each do |element|
+  #     result << element if element == block.call(element)
+  #   end
+  #   result
+  # end
+
+  # def my_map(&block)
+  #   result = []
+  #   my_each do |k|
+  #     if k == block.call(k)
+  #       result << k
+  #     end
+  #   end
+  #   result
+  # end
+end
