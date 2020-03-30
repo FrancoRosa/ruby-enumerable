3902566166e4f86875e99d0cfc79690585e3a7bc
FrancoRosa <km115.franco@gmail.com>
2020-03-30 17:53:48 -0500

    Fix my_select method

--- scripts.rb
+++ scripts.rb
@@ -8,8 +8,9 @@ module Enumerable
         yield self[i]
         i = i.next
       end
+    else
+      to_enum
     end
-    to_enum
   end
 
   def my_each_with_index
@@ -19,8 +20,9 @@ module Enumerable
         yield self[i], i
         i = i.next
       end
+    else
+      to_enum
     end
-    to_enum
   end
 
   def my_select
@@ -30,8 +32,9 @@ module Enumerable
         result << k if yield(k) == true
       end
       result
+    else
+      to_enum
     end
-    to_enum
   end
 
   # def my_map(&block)
@@ -47,9 +50,9 @@ end
 
 a = [2,756,34,68,1]
 puts "#### Embeded: ####"
-puts a.select {|k| k.odd?}
+puts a.select {|k| k.even?}
 puts "#### Mine: ####"
-puts a.my_select {|k| k.odd?}
+puts a.my_select {|k| k.even?}
 
 puts "#### Embeded: ####"
 puts a.select
