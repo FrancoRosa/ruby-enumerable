872589e2f9cbc1616cff423b031f57ad729d997d
Felipe Rosa <flp.far@gmail.com>
2020-04-06 19:13:09 -0300

    Add tests for my_select and my_all?

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -68,23 +68,54 @@ RSpec.describe Enumerable do
   end
 
   describe '#my_select' do
-    context 'Iterate over a test sample' do
-      it 'loops trough an array and compares with the argument' do
-        input = [1, 2, 3, 4, 6, 7]
-        actual = input.my_select(&:even?)
-        expected = input.select(&:even?)
-        expect(actual).to eql expected
+    it 'returns array of elements that satisfies the block condition' do
+      actual = test_array_1.my_select { |x| x == 5 }
+      expected = test_array_1.select { |x| x == 5 }
+      expect(actual).to eql expected
+    end
+
+    it 'returns a Enumerator when a block is not given' do
+      expect(test_range_1.my_select).to be_a(Enumerator)
+      expect(test_range_1.my_select.to_a).to eq test_range_1.to_a
+    end
+  end
+
+  describe 'my_all?' do
+    context 'when a block is given' do
+      it 'returns false when an element does not satisfy the block condition' do
+        expect(test_array_1.my_all? { |number| number < 56 }).to be false
       end
-      it 'loops trough an range uses a block as input' do
-        input = [1, 5, 3, 4, 5, 7]
-        actual = input.my_select { |x| x == 5 }
-        expected = input.select { |x| x == 5 }
-        expect(actual).to eql expected
+      it 'returns true when all elements satisfy the block condition' do
+        expect(test_array_1.my_all? { |number| number <= 56 }).to be true
       end
-      it 'returns an Enumerator (without a block)' do
-        input = [1, 5, 3, 4, 5, 7]
-        actual = input.my_select
-        expect(actual.class).to eq Enumerator
+    end      
+
+    context 'when an argument is given' do
+      context 'when the argument is a RegExp' do
+        it 'returns false when an element does not match the RegExp' do
+          expect(%w[ant bear cat].my_all?(/t/)).to be false
+        end
+        it 'returns true when all elements match the RegExp' do
+          expect(%w[ant goat cat].my_all?(/t/)).to be true
+        end
+      end
+      context 'when the argument is a Class' do
+        it 'returns false when an element does not belong the Class' do
+          expect([1, '2', 'y', 'x'].my_all?(String)).to be false
+        end
+        it 'returns true when all elements belong the Class' do
+          expect([1, 1.3, 2i, 4].my_all?(Numeric)).to be true
+        end
+      end
+    end
+
+    context 'when no argument or block is given' do
+      it 'returns false when an element is a false class or nil' do
+        expect([1, nil, 'y', 'x'].my_all?).to be false
+        expect([1, false, 'y', 'x'].my_all?).to be false
+      end
+      it 'returns true when all elements are not false or nil' do
+        expect([1, 1.3, 2i, 4].my_all?).to be true
       end
     end
   end
